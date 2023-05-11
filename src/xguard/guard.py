de9f59e89cf6abff42267354bc60f7dabd4d9e02
John Gitahi <gitahi109@gmail.com>
2023-05-11 22:17:43 +0300

    Fix linting issues

--- src/xguard/guard.py
+++ src/xguard/guard.py
@@ -1,39 +1,62 @@
 #!/usr/bin/python
 """
-    Written by Gitahi
-    Written on: March 31, 2022
-    Inspired by: Adrian Kuboka's NGuard, he taught me a lot about many things including GCs
-    Description: xguard is just another guard clause library, nothing new/big
+Written by Gitahi
+Written on: March 31, 2022
+Inspired by: Adrian Kuboka's NGuard, he taught me a lot about many things including GCs
+Description: xguard is just another guard clause library, nothing new/big
 """
 
+
 class Xguard:
+    """
+    A guard clause library for checking input values
+    """
 
     def not_null(self, arg):
         """
         Guard clause that raises an error if arg value is None.
-        
+
         @param arg: The input value to check
-        @type value: Any
-        
-        @raise ValueIsNoneError: if the input value is None. // #TODO: create custom errors
+        @type arg: Any
+
+        @raise ValueError: if the input value is None. // #TODO: create custom errors
         """
         if arg is None:
             raise ValueError("Value cannot be None")
 
         return self
 
-    def is_type(self, arg, argType):
-        
-        if not isinstance(arg, argType):
-            raise TypeError(f"Value must be of type {argType}")
+    def is_type(self, arg, arg_type):
+        """
+        Guard clause that raises an error if arg type is not of the
+        specified type(arg_type)
 
-        return  self
+        @param arg: The input value to check
+        @type arg: Any
 
+        @param arg_type: The type to check against
+        @type arg_type: type
 
-    def is_gt(self, arg: int, argLim: int):
-        if arg <= argLim:
-            raise TypeError("Impossible")
-        
+        @raise TypeError: if the input value is not of the specified type.
+        """
+        if not isinstance(arg, arg_type):
+            raise TypeError(f"Value must be of type {arg_type}")
+
         return self
 
-    #TODO: add more checks
+    def is_gt(self, arg: int, arg_lim: int):
+        """
+        Guard clause that raises an error if arg value is less than or equal to the specified limit.
+
+        @param arg: The input value to check
+        @type arg: int
+
+        @param arg_lim: The limit to check against
+        @type arg_lim: int
+
+        @raise TypeError: if the input value is less than or equal to the specified limit.
+        """
+        if arg <= arg_lim:
+            raise TypeError("Impossible")
+
+        return self
--- tests/test_xguard.py
+++ tests/test_xguard.py
@@ -1,25 +1,46 @@
+"""
+Unit tests for the Xguard class in the xguard package.
+"""
+
 import unittest
 from xguard import guard
 
+
 class TestXguard(unittest.TestCase):
+    """
+    Unit tests for the Xguard class.
+    """
 
     def setUp(self):
+        """
+        Initialize the guard
+        """
         self.guard = guard.Xguard()
 
     def test_not_null(self):
+        """
+        Test the not_null method of the Xguard class.
+        """
         with self.assertRaises(ValueError):
             self.guard.not_null(None)
-        self.assertEqual(self.guard.not_null('hello'), self.guard)
+        self.assertEqual(self.guard.not_null("hello"), self.guard)
 
     def test_is_type(self):
+        """
+        Test the is_type method of the Xguard class.
+        """
         with self.assertRaises(TypeError):
-            self.guard.is_type('hello', int)
+            self.guard.is_type("hello", int)
         self.assertEqual(self.guard.is_type(123, int), self.guard)
 
     def test_is_gt(self):
+        """
+        Test the is_gt method of the Xguard class.
+        """
         with self.assertRaises(TypeError):
             self.guard.is_gt(5, 10)
         self.assertEqual(self.guard.is_gt(10, 5), self.guard)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
